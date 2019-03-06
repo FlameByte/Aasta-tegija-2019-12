34085ab8a8364883ba0d5824f7a60bd467b746ea
FlameByte <45162111+FlameByte@users.noreply.github.com>
2019-03-06 11:37:05 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,7 +5,20 @@ function flash(el, c1, c2) {
 }
 var clr1 = setInterval(function() { flash('foo1', '#007bff', 'black') }, 1000);
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nupp").style.display = "block";
+  } else {
+    document.getElementById("nupp").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
 
 
 
